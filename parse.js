f796d9978058ace1e41adc380934f367cff1b197
fingerskier <fingerskier@gmail.com>
2018-09-18 07:53:22 -0700

    node parser

--- parse.js
+++ parse.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
-let data = fs.readFileSync('output')
+const path = require('path')
 
+let data = fs.readFileSync('output')
+let subdirectory = 'testdir'
+
+
+if (!fs.existsSync(subdirectory))
+    fs.mkdirSync(subdirectory)
 
 data = data.toString().split('\n')
+
+const touch = (filename, callback) => {
+    fs.openSync(filename, 'w', (err, fd) => {
+        err ? callback(err) : fs.closeSync(fd, callback)
+    })
+}
+
 
 for (let record of data) {
     fields = record.split(',')
@@ -11,15 +24,20 @@ for (let record of data) {
     let prevBook
     let bookNum = fields[1]
     let chapter = fields[2]
+    let prevChapter
     let verseNum = fields[3]
     let verse = fields[4]
-    let currFile = './' + book + '/' + chapter
 
-    if (book !== prevBook) fs.mkdirSync(book)
+    let directory = `./${subdirectory}/${bookNum}`
+    let currFile = `${directory}/${chapter}`
 
-    if (chapter !== prevChapter) fs.mkdirSync(currFile)
 
-    fs.appendFileSync(currFile, verse)
+    if (book !== prevBook)
+        if (!fs.existsSync(directory))
+            fs.mkdirSync(directory)
+
+
+   fs.appendFileSync(currFile, verse)
 
 
     prevBook = book
